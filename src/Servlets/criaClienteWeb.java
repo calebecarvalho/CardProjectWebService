2f5cb4dafbe89384b6e59466b1db17cd792a6f81
Calebe Lustosa Carvalho <calebecarvalho@users.noreply.github.com>
2016-12-10 16:35:41 -0200

    New classes

---
+++ src/Servlets/criaClienteWeb.java
@@ -0,0 +1,56 @@
+package Servlets;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import MySQLConnector.ClientConnector;
+import MySQLTranslator.ClienteTranslator;
+import com.google.gson.Gson;
+import ApacheComplements.Complement;
+/**
+ * Created by CalebeLustosa on 09/12/2016.
+ */
+@WebServlet(name = "criaClienteWeb", urlPatterns = {"/criaClienteWeb"})
+public class criaClienteWeb extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        try{
+
+            PrintWriter saida = response.getWriter();
+            response.setHeader("Content-Type", "application/json");
+            Complement complement = new Complement();
+            ClienteTranslator clienteTranslator = new ClienteTranslator();
+            ClientConnector cliente = new ClientConnector();
+
+            String confSenha = request.getParameter("confirmacao");
+            if(!(request.getParameter("senha").equals(confSenha))){
+                request.setAttribute("ERRO", "Repita a mesma senha nos campos");
+                RequestDispatcher dispatcher = request.getRequestDispatcher("inicial.jsp");
+                dispatcher.forward(request, response);
+            }
+
+            cliente.setNome(request.getParameter("nome"));
+            cliente.setCpf(complement.normaliza(request.getParameter("cpf_insert")));
+            cliente.setSenha(request.getParameter("senha"));
+            cliente.setCep(request.getParameter("cep"));
+            cliente.setEnd(request.getParameter("end"));
+            cliente.setEmail(request.getParameter("email"));
+            cliente.setTelefone(request.getParameter("telefone"));
+            cliente.setCelular(request.getParameter("celular"));
+            clienteTranslator.cadastrarCliente(cliente);
+            RequestDispatcher dispatcher = request.getRequestDispatcher("iniciar.jsp");
+            dispatcher.forward(request,response);
+
+        }catch (Exception E){
+            E.getStackTrace();
+        }
+    }
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.sendRedirect("/temp/erro.jsp");
+    }
+}
---
+++ src/Servlets/verificaUsuarioWeb.java
@@ -0,0 +1,56 @@
+package Servlets;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import MySQLTranslator.ClienteTranslator;
+import ApacheComplements.Complement;
+
+/**
+ * Created by CalebeLustosa on 10/12/2016.
+ */
+@WebServlet(name = "verificaUsuarioWeb")
+public class verificaUsuarioWeb extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    try {
+        PrintWriter saida = response.getWriter();
+        response.setHeader("Content-Type", "application/json");
+        Complement complement = new Complement();
+        String CPF = complement.normaliza(request.getParameter("CPF"));
+        String senha = request.getParameter("senha");
+
+        if (CPF == null){
+            request.setAttribute("ERRO","Campo de CPF não pode ser vazio");
+            RequestDispatcher dispatcher = request.getRequestDispatcher("validacao.jsp");
+            dispatcher.forward(request,response);
+        }
+        if (senha == null){
+            request.setAttribute("ERRO","Campo de senha não pode ser vazio");
+            RequestDispatcher dispatcher = request.getRequestDispatcher("validacao.jsp");
+            dispatcher.forward(request,response);
+        }
+        ClienteTranslator clienteTranslator = new ClienteTranslator();
+        boolean sucesso = clienteTranslator.verificaAcessoCliente(CPF,senha);
+
+        if(sucesso){
+            RequestDispatcher dispatcher = request.getRequestDispatcher("inical.jsp");
+
+        }else {
+            request.setAttribute("ERRO","Informações de login inválidos");
+            RequestDispatcher dispatcher = request.getRequestDispatcher("inicial.jsp");
+            dispatcher.forward(request,response);
+        }
+
+    }catch (Exception E){
+        E.getStackTrace();
+    }
+}
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.sendRedirect("/temp/erro.jsp");
+    }
+}
