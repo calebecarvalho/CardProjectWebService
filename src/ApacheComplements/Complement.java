aafd895c85f8bfbbfb6446ddec27944300c26668
Calebe Lustosa Carvalho <calebecarvalho@users.noreply.github.com>
2016-12-11 11:07:06 -0200

    Fixes
    Signed-off-by: Calebe Lustosa Carvalho <yoshke23@gmail.com>

--- src/ApacheComplements/Complement.java
+++ src/ApacheComplements/Complement.java
@@ -8,37 +8,24 @@ import java.text.ParseException;
  */
 public class Complement {
 
-    public String normaliza (String CPF){
+    public String normaliza (String CPF) {
         String nrml = CPF;
-        while(nrml.indexOf("-") != -1){
-            if (nrml.indexOf("-") != 0){
-                nrml = nrml.substring(0, nrml.indexOf("-"))+
-                        nrml.substring(nrml.indexOf("-")+1);
-            }else{
-                nrml = nrml.substring(nrml.indexOf("-")+1);
+        while (nrml.indexOf("-") != -1) {
+            if (nrml.indexOf("-") != 0) {
+                nrml = nrml.substring(0, nrml.indexOf("-")) +
+                        nrml.substring(nrml.indexOf("-") + 1);
+            } else {
+                nrml = nrml.substring(nrml.indexOf("-") + 1);
             }
         }
         while (nrml.indexOf(".") != -1) {
             if (nrml.indexOf(".") != 0) {
                 nrml = nrml.substring(0, nrml.indexOf(".")) +
                         nrml.substring(nrml.indexOf(".") + 1);
-            }
-            else {
+            } else {
                 nrml = nrml.substring(nrml.indexOf(".") + 1);
             }
         }
         return nrml;
-    }
-    public static Date normalizaData(String data) throws Exception {
-        if (data == null || data.equals(""))
-            return null;
-        Date date = null;
-        try {
-            DateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
-            date = (java.sql.Date)formatter.parse(data);
-        } catch (ParseException E) {
-            throw E;
-        }
-        return date;
     }
 }
